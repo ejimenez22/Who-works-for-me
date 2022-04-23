b036ea0dc52e9874e26d089c84e9a1b77af6f6bc
Enrique Jimenez <ejimenez2292@gmail.com>
2022-04-22 20:35:01 -0500

    setup schema and seeds sql with base db, next working on prompts

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO department (id, name)
+VALUES
+(1, 'Mangement'),
+(2, 'Developers'),
+(3, 'Maintenance');
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES
+(1, 'Executive Director', '200000.00', 1),
+(2, 'Lead Developer', '150000.00', 2),
+(3, 'Developer', '120000.00', 3),
+(4, 'Tech Maintenance', '100000.00', 4),
+(5, 'Janitorial Director', '25000.00', 5);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES
+(1, 'Enrique', 'Jimenez', 1, 0),
+(2, 'Chris', 'Humphrey', 2, 1),
+(3, 'Joe', 'Gagliardo', 3, 2),
+(4, 'Tony', 'Sabatino', 3, 2),
+(5, 'Eric', 'Humphrey', 4, 2),
+(6, 'Jared', 'Humphrey', 5, 2);
