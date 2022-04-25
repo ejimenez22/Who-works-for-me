8147671c1737d7b67a20119bf57a60ac0799860d
Enrique Jimenez <ejimenez2292@gmail.com>
2022-04-24 19:27:05 -0500

    added fk to schema

--- db/schema.sql
+++ db/schema.sql
@@ -11,13 +11,14 @@ CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
-    department_id INTEGER NOT NULL REFERENCES(department.id)   
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
     );
 
 CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL REFERENCES(role.id),
-    manager_id INTEGER NOT NULL REFERENCES(employee.id)
+    role_id INTEGER NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id)
     );
